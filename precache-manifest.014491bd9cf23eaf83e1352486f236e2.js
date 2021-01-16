d2146519275cf8aaa9498f1bf9d82b90141676fe
MesonWarrior <chtonibudto@yandex.ru>
2021-01-16 22:59:47 +0300

    Add files via upload

---
+++ precache-manifest.014491bd9cf23eaf83e1352486f236e2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b3c72f3b4931167c5144ab680deeab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c01b2e86a43cfcff01b",
+    "url": "./static/css/main~d0ae3f07.7c84d9e9.chunk.css"
+  },
+  {
+    "revision": "43e868fa800077da7db3",
+    "url": "./static/css/vendor~f9ca8911.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6c01b2e86a43cfcff01b",
+    "url": "./static/js/main~d0ae3f07.95d38790.chunk.js"
+  },
+  {
+    "revision": "0d971da5963a0d6f1962",
+    "url": "./static/js/runtime-main.19d5ae45.js"
+  },
+  {
+    "revision": "eaf57a12f23e59ee445a",
+    "url": "./static/js/vendor~19c11a8a.2d968c41.chunk.js"
+  },
+  {
+    "revision": "8df9e60cb3ecae489f95",
+    "url": "./static/js/vendor~253ae210.d7835722.chunk.js"
+  },
+  {
+    "revision": "78703dcf1d1f1986ef2cb2d3cd1f72a2",
+    "url": "./static/js/vendor~253ae210.d7835722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93bd1501e8ba88c02928",
+    "url": "./static/js/vendor~70a3a3d1.f55bfa09.chunk.js"
+  },
+  {
+    "revision": "8862bfc0832c42970e65",
+    "url": "./static/js/vendor~a57fface.86aeb138.chunk.js"
+  },
+  {
+    "revision": "6e4c347705a04c8cda9b",
+    "url": "./static/js/vendor~ae58bffc.1a05f69a.chunk.js"
+  },
+  {
+    "revision": "c15997f1d7d0a4c3c2dd",
+    "url": "./static/js/vendor~b1c0def3.199a1501.chunk.js"
+  },
+  {
+    "revision": "dc1bff1dedbfd99d45aa",
+    "url": "./static/js/vendor~bc04a12f.d47e2a79.chunk.js"
+  },
+  {
+    "revision": "43e868fa800077da7db3",
+    "url": "./static/js/vendor~f9ca8911.73e81209.chunk.js"
+  },
+  {
+    "revision": "3d720969d688b8ad9b3cecee0b67fee4",
+    "url": "./static/js/vendor~f9ca8911.73e81209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98c020f50ba6666f49dedc9469fbd2c",
+    "url": "./static/media/pencil.c98c020f.svg"
+  },
+  {
+    "revision": "955165b08a78075f90ddbf4393a500ad",
+    "url": "./static/media/trash.955165b0.svg"
+  }
+]);
